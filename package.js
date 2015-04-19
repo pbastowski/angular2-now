71a1ce287c00842f7a3122f6d4be1bb1e31c0699
Bastowski, PL (ITCDCC) - KLM <Paul.Bastowski@klm.com>
2015-04-19 15:55:40 +0200

    added GIT repo URL to package.js

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Use Angular2 @Component syntax with Angular 1.x and Babel',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git:     'https://github.com/pbastowski/angular2-now.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
