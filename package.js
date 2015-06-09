37c1c2c842c41706a0459146391e2bc218cb7ce3
Paul Bastowski <pbastowski@gmail.com>
2015-06-09 17:46:53 +0200

    Bumped version to 0.2.7.

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ Package.describe({
   summary: 'Use Angular2 @Component syntax with Angular 1.x and Babel',
   // URL to the Git repository containing the source code for this package.
   git:     'https://github.com/pbastowski/angular2-now.git',
-  // By default, Meteor will default to using README.md for documentation.
+  // By default,  Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
