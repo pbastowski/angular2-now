9442c2a30f829e26bafe84980451f1fe253b451a
Paul Bastowski <pbastowski@gmail.com>
2015-06-20 20:32:39 +0200

    bumped version to 0.2.9

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'pbastowski:angular2-now',
-  version: '0.2.8',
-  // Brief, one-line summary of the package.
+  version: '0.2.9',
   summary: 'Use Angular2 @Component syntax with Angular 1.x and Babel',
-  // URL to the Git repository containing the source code for this package.
   git:     'https://github.com/pbastowski/angular2-now.git',
-  // By default,  Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
