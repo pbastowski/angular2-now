09ab9e60eb31a6da535ffdd47cc3146c251ca957
Robert Baker <se7enalive@gmail.com>
2015-07-30 22:39:33 -0700

    Create angular2now.d.ts
    initial typescript definitions

---
+++ angular2now.d.ts
@@ -0,0 +1,88 @@
+/// <reference path="../angularjs/angular.d.ts" />
+
+declare var bootstrap: typeof angular2now.bootstrap;
+declare var SetModule: typeof angular2now.SetModule;
+
+declare module 'angular2now' {
+    export var Filter: typeof angular2now.Filter;
+    export var State: typeof angular2now.State;
+    export var Component: typeof angular2now.Component;
+    export var Service: typeof angular2now.Service;
+    export var View: typeof angular2now.View;
+    export var options: typeof angular2now.options;
+
+    //export var bootstrap: typeof angular2now.bootstrap;
+    //export var SetModule: typeof angular2now.SetModule;
+
+    export var Inject: typeof angular2now.Inject;
+}
+
+declare module angular2now {
+    // Class decorators.
+    export function Component(config: ComponentConfig): ClassDecorator;
+
+    export function Service(config: ServiceConfig): ClassDecorator;
+
+    export function Filter(config: FilterConfig): ClassDecorator;
+
+    export function State(config: StateConfig): ClassDecorator;
+
+    export function View(config: ViewConfig): ClassDecorator;
+
+    export function bootstrap(appName: any, dependencies?: string[]);
+
+    export function SetModule(appName: string, dependencies?: string[]);
+
+    export function options(config: options);
+
+    export function Inject(dependencies: string[]);
+
+    interface options {
+        controllerAs?: string;
+        spinner?: any;
+        events?: any;
+        noConflict?: boolean;
+    }
+
+    interface ServiceConfig {
+        name: string;
+    }
+
+    interface StateConfig {
+        name: string;
+        url?: string;
+        defaultRoute?: boolean|string;
+        resolve?: any;
+        controller?: ControllerClass;
+        template?: any;
+        html5Mode?: boolean;
+    }
+
+    interface ViewConfig {
+        template?: string;
+        templateUrl?: string;
+    }
+
+    interface ComponentConfig {
+        selector: string;
+        bind?: Bind;
+        injectables?: string[];
+        require?: string;
+        transclude?: boolean;
+    }
+
+    interface Bind {
+        [id: string]: any;
+    }
+
+    interface FilterConfig {
+        name: string;
+    }
+
+    interface ControllerClass extends Function {
+        template?: string|Function;
+        templateUrl?: string|Function;
+        link?: Function;
+        compile?: any;
+    }
+}
