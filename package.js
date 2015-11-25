726cfde93889043a1ac48af98d67de3475745cb1
Paul Bastowski <pbastowski@gmail.com>
2015-11-26 00:29:39 +0100

    v1.0.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name:          'pbastowski:angular2-now',
-    version:       '1.0.0',
+    version:       '1.0.1',
     summary:       'Angular 2 @Component syntax for Meteor 1.2 and AngularJS',
     git:           'https://github.com/pbastowski/angular2-now/tree/meteor1.2',
     documentation: 'README.md'
