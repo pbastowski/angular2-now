d1d0d16f569a774f5d0ad06d282021e0232c4dab
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2015-12-23 19:54:19 +0100

    test(ScopeShared): basic functionality

--- angular2-now-spec.js
+++ angular2-now-spec.js
@@ -481,4 +481,34 @@ describe("angular2-now", () => {
       expect(result).toBe(Target);
     });
   });
+
+  describe("@ScopeShared()", () => {
+    it("should set scope as undefined", () => {
+      const target = {};
+
+      angular2now.ScopeShared(target);
+
+      expect(target.scope).toBeUndefined();
+    });
+
+    it("should overwrite scope to undefined", () => {
+      const target = {
+        scope: true
+      };
+
+      angular2now.ScopeShared(target);
+
+      expect(target.scope).toBeUndefined();
+    });
+
+    it("should return target", () => {
+      const target = {
+        scope: true
+      };
+
+      const result = angular2now.ScopeShared(target);
+
+      expect(result).toBe(target);
+    });
+  });
 });
