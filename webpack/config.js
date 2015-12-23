5036b858e794b601253ee1714291012535b5b5c0
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2015-12-23 14:20:35 +0100

    chore: webpack configuration
    I created test.js and put there whole configuration. It allows for use
    webpack with other configurations (development or production use)

---
+++ webpack/config.js
@@ -0,0 +1,3 @@
+var test = require('./test');
+
+module.exports = test;
---
+++ webpack/test.js
@@ -0,0 +1,29 @@
+module.exports = {
+    entry: './angular2-now-spec.js',
+    stats: {
+        colors: true,
+        reasons: true
+    },
+    resolve: {
+        extensions: ["", ".js"]
+    },
+    module: {
+        loaders: [
+            // transpile all files except testing sources with babel as usual
+            {
+                test: /\.js$/,
+                include: [
+                    /-spec/
+                ],
+                exclude: /node_modules/,
+                loader: 'babel'
+            },
+            // transpile and instrument only testing sources with isparta
+            {
+                test: /angular2-now\.js$/,
+                exclude: /node_modules/,
+                loader: 'isparta'
+            }
+        ]
+    }
+};
