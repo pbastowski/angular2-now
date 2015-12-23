4f80712a19e33024354851e7cbe25d40e9462d30
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2015-12-23 17:26:00 +0100

    chore: add underscore to tests

--- karma.config.js
+++ karma.config.js
@@ -30,6 +30,7 @@ module.exports = function(config) {
       'node_modules/angular/angular.js',
       'node_modules/angular-mocks/angular-mocks.js',
       'node_modules/angular-ui-router/release/angular-ui-router.js',
+      'node_modules/underscore/underscore.js',
       testFile
     ],
 
