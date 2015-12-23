aa3493ceea0637be081fd6f0cb5f45b19a591257
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2015-12-23 18:40:50 +0100

    test(Controller): basic functionality

--- angular2-now-spec.js
+++ angular2-now-spec.js
@@ -246,4 +246,75 @@ describe("angular2-now", () => {
       expect(target.directives).toBe(directives);
     });
   });
+
+  describe("@Controller()", () => {
+    const name = 'TestCtrl';
+    const moduleMock = {
+      controller: function() {}
+    }
+    let spy;
+    let spyCtrl;
+    function Target() {};
+
+
+    beforeEach(() => {
+      // set spies
+      spy = spyOn(angular, 'module').and.returnValue(moduleMock);
+      spyCtrl = spyOn(moduleMock, 'controller');
+    });
+
+    describe("with namespace", () => {
+      beforeEach(() => {
+        // set ngModuleName as currentModule
+        angular2now.SetModule(`ns:${ngModuleName}`);
+      });
+
+      it("should set name if argument is a string", () => {
+        angular2now.Controller(name)(Target);
+
+        expect(spy).toHaveBeenCalledWith(ngModuleName);
+        expect(spyCtrl).toHaveBeenCalledWith(`ns_${name}`, Target);
+      });
+
+      it("should set name if argument is an object with name property", () => {
+        angular2now.Controller({
+          name
+        })(Target);
+
+        expect(spy).toHaveBeenCalledWith(ngModuleName);
+        expect(spyCtrl).toHaveBeenCalledWith(`ns_${name}`, Target);
+      });
+    });
+
+    describe("without namespace", () => {
+      beforeEach(() => {
+        // set ngModuleName as currentModule
+        angular2now.SetModule(`:${ngModuleName}`);
+      });
+
+      it("should set name if argument is a string", () => {
+        angular2now.Controller(name)(Target);
+
+        expect(spy).toHaveBeenCalledWith(ngModuleName);
+        expect(spyCtrl).toHaveBeenCalledWith(name, Target);
+      });
+
+      it("should set name if argument is an object with name property", () => {
+        angular2now.Controller({
+          name
+        })(Target);
+
+        expect(spy).toHaveBeenCalledWith(ngModuleName);
+        expect(spyCtrl).toHaveBeenCalledWith(name, Target);
+      });
+    });
+
+    it("should return target", () => {
+      const result = angular2now.Controller({
+        name
+      })(Target);
+
+      expect(result).toBe(Target);
+    });
+  });
 });
