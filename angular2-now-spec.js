99f3d0e5c94181525345cfa1d19615b3aa881857
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2015-12-23 17:26:24 +0100

    test(Inject): basic functionality

--- angular2-now-spec.js
+++ angular2-now-spec.js
@@ -29,7 +29,7 @@ describe("angular2-now", () => {
       expect(angular2now.SetModule(ngModuleName)).toBe(angular.module(ngModuleName));
     });
 
-    it("should overwrite module when using namespace with the same module name", () => {
+    it("should overwrite module when using namespace and the same module name", () => {
       const nsModule = `ns:${ngModuleName}`
       angular2now.SetModule(nsModule, []);
 
@@ -45,6 +45,83 @@ describe("angular2-now", () => {
       const newModule = `${ngModuleName}New`
       angular2now.SetModule(newModule, []);
       expect(angular2now.options().currentModule()).toBe(newModule);
+    });
+  });
+
+  describe("@Inject()", () => {
+    // TODO add tests of null, undefined and empty array as Inject() argument
+    it("should throw error on empty dependencies", () => {
+      expect(() => {
+        angular2now.Inject();
+      }).toThrowError(Error, /dependencies/);
+    });
+
+    it("should pass dependencies as an array", () => {
+      expect(() => {
+        angular2now.Inject(['$http', '$q']);
+      }).not.toThrowError(Error, /dependencies/);
+    });
+
+    it("should pass dependencies directly", () => {
+      expect(() => {
+        angular2now.Inject('$http', '$q');
+      }).not.toThrowError(Error, /dependencies/);
+    });
+
+    it("should fail on missing target", () => {
+      expect(() => {
+        angular2now.Inject(['$http', '$q'])();
+      }).toThrowError(TypeError, /class/);
+    });
+
+    describe("with target", () => {
+      // Services
+      const Injectables = {
+        target: ['$log', '$q'],
+        inject: ['$http', '$http', '$q', 'customService']
+      };
+      const concatedRaw = Injectables.inject.concat(Injectables.target);
+      const concated = _.uniq(
+        _.reject(concatedRaw, (inj) => inj[0] !== '$')
+      );
+      // target mock
+      const TargetMock = {
+        $inject: Injectables.target
+      };
+      // @Inject mock
+      const InjectMock = angular2now.Inject(Injectables.inject);
+
+      it("should return target", () => {
+        expect(InjectMock(TargetMock)).toBe(TargetMock);
+      });
+
+      // TODO fix bug! There are duplicates if target contains injectables and there is the same injectable provided by @Inject()
+      it("should concat injectables", () => {
+        // check if all injectables have been added
+        concated.forEach((inj) => {
+          expect(TargetMock.$inject.indexOf(inj)).not.toBe(-1);
+        });
+
+      });
+
+      it("should add namespace to injectables", () => {
+        const namespaced = _.reject(concatedRaw, (inj) => inj[0] === '$');
+
+        // check with namespace prefix
+        namespaced.forEach((inj) => {
+          expect(TargetMock.$inject.indexOf(`ns_${inj}`)).not.toBe(-1);
+        });
+      });
+
+      // TODO wait for pbastowski response
+      /*
+      it("should prevent duplicates", () => {
+        const uniqueConcated = _.uniq(concatedRaw);
+        const uniqueTarget = _.uniq(TargetMock.$inject);
+        const diff = _.difference(uniqueTarget, uniqueConcated);
+
+        expect(diff.length).toBe(0);
+      });*/
     });
   });
 });
