bbbc9416cf9f2e4acdb24cd56558a04814f7945c
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2015-12-24 12:48:46 +0100

    chore: add jasmine html reporter (see debug.html)

--- karma.config.js
+++ karma.config.js
@@ -62,7 +62,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
+    reporters: ['progress', 'coverage', 'html'],
 
 
     // web server port
