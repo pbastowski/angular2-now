f75d197befb9bd46ad6cc0591900eac5359af3ed
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-01-01 17:39:22 +0100

    style(Service)

--- src/api/service.js
+++ src/api/service.js
@@ -1,16 +1,19 @@
 import { common } from './../common';
 import { nameSpace } from './../utils';
+
 export function Service(options) {
-    options = options || {};
-    // Allow shorthand notation of just passing the name as a string
-    if (typeof options === 'string')
-        options = { name: options };
-
-    return function (target) {
-        angular.module(common.currentModule)
-            .service(nameSpace(options.name), target);
-        //.factory(options.name, function () { return new target })
-
-        return target;
+  options = options || {};
+  // Allow shorthand notation of just passing the name as a string
+  if (typeof options === 'string')
+    options = {
+      name: options
     };
+
+  return function(target) {
+    angular.module(common.currentModule)
+      .service(nameSpace(options.name), target);
+    //.factory(options.name, function () { return new target })
+
+    return target;
+  };
 }
