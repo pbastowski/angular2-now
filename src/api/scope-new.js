9c7a7a92fab503d0f3ceff498fe51743a39e7925
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-01-01 17:39:00 +0100

    style(ScopeNew)

--- src/api/scope-new.js
+++ src/api/scope-new.js
@@ -1,6 +1,6 @@
 // Requests a new scope to be created when the directive is created.
 // The other way to do this is to pass "scope: true" to @Component.
-export function ScopeNew (target) {
-    target.scope = true;
-    return target
+export function ScopeNew(target) {
+  target.scope = true;
+  return target
 }
