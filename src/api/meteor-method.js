dadf05ab0dda17ff48c6c5aad9777180fe0b1c88
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-01-01 17:36:06 +0100

    refactor(MeteorMethod): transform to es6

--- src/api/meteor-method.js
+++ src/api/meteor-method.js
@@ -2,57 +2,66 @@ import { common } from './../common';
 
 // The name of the Meteor.method is the same as the name of class method.
 export function MeteorMethod(_options) {
-    var options = angular.merge({}, common.ng2nOptions, _options);
-    var spinner = options.spinner || {show: angular.noop, hide: angular.noop};
-    var events = options.events || {beforeCall: angular.noop, afterCall: angular.noop};
+  const options = angular.merge({}, common.ng2nOptions, _options);
+  let spinner = options.spinner || {
+    show: angular.noop,
+    hide: angular.noop
+  };
+  const events = options.events || {
+    beforeCall: angular.noop,
+    afterCall: angular.noop
+  };
 
-    return function (target, name, descriptor) {
+  return function(target, name, descriptor) {
 
-        // Create a method that calls the back-end
-        descriptor.value = function () {
-            var argv = Array.prototype.slice.call(arguments);
-            var deferred = common.$q.defer();
+    // Create a method that calls the back-end
+    descriptor.value = function() {
+      const argv = Array.prototype.slice.call(arguments);
+      const deferred = common.$q.defer();
 
-            if (typeof spinner === 'string') {
-                if (angular.injector(['ng', common.currentModule]).has(options.spinner)) {
-                    spinner = angular.injector(['ng', common.currentModule]).get(options.spinner);
-                    options.spinner = spinner;
-                } else
-                    throw new Error('Spinner "' + spinner + '" does not exist.');
-            }
+      if (typeof spinner === 'string') {
+        if (angular.injector(['ng', common.currentModule]).has(options.spinner)) {
+          spinner = angular.injector(['ng', common.currentModule]).get(options.spinner);
+          options.spinner = spinner;
+        } else
+          throw new Error('Spinner "' + spinner + '" does not exist.');
+      }
 
-            argv.unshift(name);
-            argv.push(resolver);
+      argv.unshift(name);
+      argv.push(resolver);
 
-            if (spinner) spinner.show();
-            if(events.beforeCall) {
-              events.beforeCall();
-            }
-            // Call optional events.beforeCall()
+      if (spinner) {
+        spinner.show();
+      }
 
-            // todo: should call Meteor after resolution of promise returned by beforeCall()
-            Meteor.call.apply(this, argv);
+      if (events.beforeCall) {
+        events.beforeCall();
+      }
+      // Call optional events.beforeCall()
 
-            deferred.promise.finally(function() {
-                spinner.hide();
-                // Call optional events.afterCall()
-                if(events.afterCall) {
-                  events.afterCall();
-                }
+      // todo: should call Meteor after resolution of promise returned by beforeCall()
+      Meteor.call.apply(this, argv);
 
-            });
+      deferred.promise.finally(function() {
+        spinner.hide();
+        // Call optional events.afterCall()
+        if (events.afterCall) {
+          events.afterCall();
+        }
 
-            return deferred.promise;
+      });
 
-            function resolver(err, data) {
-                if (err)
-                    deferred.reject(err);
-                else
-                    deferred.resolve(data);
-            }
-        };
+      return deferred.promise;
 
-        return descriptor;
-    }
+      function resolver(err, data) {
+        if (err) {
+          deferred.reject(err);
+        } else {
+          deferred.resolve(data);
+        }
+      }
+    };
 
+    return descriptor;
+  }
 }
