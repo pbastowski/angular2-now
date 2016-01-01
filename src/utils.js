1aff446723870414fa42632382a81d33664e4c95
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-01-01 18:00:38 +0100

    refactor: use es6 on utils and refactor it

--- src/utils.js
+++ src/utils.js
@@ -3,14 +3,7 @@ import { common } from './common';
 // Create a new name from the concatenation of
 // the currentNameSpace and the name argument
 export function nameSpace(name) {
-    var nsName = name;
-
-    if (common.currentNameSpace) {
-        //nsName = camelCase(currentModule) + '.' + name;
-        nsName = common.currentNameSpace + '_' + name;
-    }
-
-    return nsName;
+  return common.currentNameSpace ? common.currentNameSpace + '_' + name : name;
 }
 
 // Does a provider with a specific name exist in the current module
@@ -19,26 +12,23 @@ export function serviceExists(serviceName) {
 }
 
 export function getService(serviceName, moduleName) {
-    if (!moduleName)
-        moduleName = common.currentModule;
-
-    return angular.module(moduleName)
+    return angular.module(moduleName || common.currentModule)
         ._invokeQueue
-        .filter(function (v, i) {
-            return v[0] === '$provide' && v[2][0] === serviceName
-        })[0];
+        .filter((v, i) => v[0] === '$provide' && v[2][0] === serviceName)[0];
 }
 
 export function camelCase(s) {
-    return s.replace(/-(.)/g, function (a, b) {
-        return b.toUpperCase();
-    });
+    return s.replace(/-(.)/g, (a, b) => b.toUpperCase());
 }
 
 export function unCamelCase(c) {
-    var s = c.replace(/([A-Z])/g, '-$1')
+    const s = c.replace(/([A-Z])/g, '-$1')
              .replace(/--/g, '-')
              .toLowerCase();
-    if (s[0] === '-') s = s.slice(1);
+
+    if (s[0] === '-') {
+      return s.slice(1);
+    }
+
     return s;
 }
