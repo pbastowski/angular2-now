0d83c4da107399f5c6a460f76029a1a489228108
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-01-01 17:27:55 +0100

    refactor(bootstrap): transform to es6

--- src/api/bootstrap.js
+++ src/api/bootstrap.js
@@ -11,11 +11,13 @@ import { common } from './../common';
  * @param ?config   angular.bootstrap() config object, see AngularJS doco
  */
 export function bootstrap(target, config) {
+  let bootOnDocument = false;
+
   if (!target || (target && !target.selector && typeof target === 'function')) {
     target = {
       selector: common.currentModule
     };
-    var bootOnDocument = true;
+    bootOnDocument = true;
   }
 
   // Allow string shortcut for target.selector. Can be the name of any HTML tag.
@@ -29,7 +31,7 @@ export function bootstrap(target, config) {
   // to handle it correctly.
   target.bootstrap = true;
 
-  var bootModule = target.selector || common.currentModule;
+  const bootModule = target.selector || common.currentModule;
 
   if (bootModule !== common.currentModule)
     angular.module(bootModule);
@@ -45,12 +47,16 @@ export function bootstrap(target, config) {
     angular.element(document).ready(onReady);
 
   function onReady() {
+    let el;
+
     // Find the component's element
-    if (!bootOnDocument)
-      var el = document.querySelector(target.selector);
+    if (!bootOnDocument) {
+      el = document.querySelector(target.selector);
+    }
     // Or use document, if user passed no arguments
-    else
-      var el = document.body;
+    else {
+      el = document.body;
+    }
 
     angular.bootstrap(el, [bootModule], config);
   }
