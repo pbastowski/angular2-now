d71cff40a6735f4973b65a811a00c6942d4813ff
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-01-02 16:24:59 +0100

    refactor(Service): compatible with eslint

--- src/api/service.js
+++ src/api/service.js
@@ -4,15 +4,16 @@ import { nameSpace } from './../utils';
 export function Service(options) {
   options = options || {};
   // Allow shorthand notation of just passing the name as a string
-  if (typeof options === 'string')
+  if (typeof options === 'string') {
     options = {
       name: options
     };
+  }
 
-  return function(target) {
+  return function ServiceTarget(target) {
     angular.module(common.currentModule)
       .service(nameSpace(options.name), target);
-    //.factory(options.name, function () { return new target })
+    // .factory(options.name, function () { return new target })
 
     return target;
   };
