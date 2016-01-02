9a86b8c58837fdf252c2a3e269684da4d6d76d03
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-01-02 16:55:47 +0100

    test: window scope

--- tests/basic-spec.js
+++ tests/basic-spec.js
@@ -1,7 +1,10 @@
 export default (angular2now, ngModuleName) => {
-  describe("basic", () => {
-    it("should be defined", () => {
+  describe('basic', () => {
+    it('should be defined', () => {
       expect(angular2now).toBeDefined();
+    });
+    it('should be available in window scope', () => {
+      expect(window.angular2now).toBeDefined();
     });
   });
 };
