f2e69b86c6aee56b0c9e589252471f77d4bba475
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-01-03 13:26:31 +0100

    chore: npm dependency as source

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
-var gulp = require('gulp')
-var replace = require('gulp-replace')
+var gulp = require('gulp');
+var replace = require('gulp-replace');
 
 // bump npm package version into package.js
 gulp.task('meteor', function() {
-  var pkg = require('./package.json')
-  var versionRegex = /(version\:\s*\')([^\']+)\'/gi
+  // get version of npm package
+  var version = require('./package.json').version;
+  // find: version: '...'
+  var versionRegex = /(version\:\s*\')([^\']+)\'/gi;
+  // find: 'angular2-now': '...'
+  var npmVersionRegex = /(\'angular2-now\'\:\s*')([^\']+)\'/gi;
 
   return gulp.src('package.js')
-    .pipe(replace(versionRegex, '$1' + pkg.version + "'"))
-    .pipe(gulp.dest('./'))
-})
+    // update version of meteor package
+    .pipe(replace(versionRegex, '$1' + version + "'"))
+    // update version of npm dependency
+    .pipe(replace(npmVersionRegex, '$1' + version + "'"))
+    .pipe(gulp.dest('./'));
+});
--- package.js
+++ package.js
@@ -6,20 +6,32 @@ Package.describe({
   documentation: 'README.md'
 });
 
-Package.onUse(function (api) {
+Npm.depends({
+  'angular2-now': '1.1.0'
+});
+
+Package.onUse(function(api) {
   api.versionsFrom('1.2.0.1');
   api.use('angular@1.3.1', 'client');
   api.imply('angular@1.3.1', 'client');
 
   // Make sure we load after pbastowski:systemjs, if it's used
-  api.use('pbastowski:systemjs@0.0.1', 'client', {weak: true});
+  api.use('pbastowski:systemjs@0.0.1', 'client', {
+    weak: true
+  });
 
-  api.addFiles(['dist/angular2-now.js', 'src/exports.js'], ['client'], {transpile: false});
+  api.addFiles([
+    '.npm/package/node_modules/angular2-now/dist/angular2-now.js',
+    'src/exports.js'
+  ], 'client', {
+    transpile: false
+  });
+
   api.export(['angular2now']);
 });
 
-Package.onTest(function (api) {
+Package.onTest(function(api) {
   api.use('tinytest');
   api.use('pbastowski:angular2-now');
-  api.addFiles('tests/meteor-spec.js', ['client']);
+  api.addFiles('tests/meteor-spec.js', 'client');
 });
