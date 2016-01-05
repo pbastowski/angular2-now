fd942309e43e5d1fa9faa13f830be1583b926ba0
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-01-05 23:58:39 +0100

    chore: set package's git property to master branch

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'pbastowski:angular2-now',
   version: '1.1.3',
   summary: 'Angular 2 @Component syntax for Meteor 1.2 and AngularJS',
-  git: 'https://github.com/pbastowski/angular2-now/tree/meteor1.2',
+  git: 'https://github.com/pbastowski/angular2-now.git',
   documentation: 'README.md'
 });
 
